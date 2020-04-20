ae157daecf9e0f49c2580106d939fb59dfc673ea
Aditya Phra <deace.inc@gmail.com>
2020-04-21 02:15:50 +0700

    update fallback resolver to 9.9.9.9

--- service.sh
+++ service.sh
@@ -11,8 +11,8 @@ fi
 $MODDIR/system/bin/dnscrypt-proxy -config $CONFIG_FILE -logfile $LOG_FILE > /dev/null 2>&1 &
 
 if [ "$AUTO_REDIRECTION" = "true" ]; then
-  iptables -t nat -A OUTPUT -p tcp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
-  iptables -t nat -A OUTPUT -p udp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
-  ip6tables -t nat -A OUTPUT -p tcp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination [::1]:5354
-  ip6tables -t nat -A OUTPUT -p udp ! -d 1.1.1.1 --dport 53 -j DNAT --to-destination [::1]:5354
+  iptables -t nat -A OUTPUT -p tcp ! -d 9.9.9.9 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+  iptables -t nat -A OUTPUT -p udp ! -d 9.9.9.9 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+  ip6tables -t nat -A OUTPUT -p tcp ! -d 9.9.9.9 --dport 53 -j DNAT --to-destination [::1]:5354
+  ip6tables -t nat -A OUTPUT -p udp ! -d 9.9.9.9 --dport 53 -j DNAT --to-destination [::1]:5354
 fi
